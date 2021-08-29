95fc60527b034a97c68a33a0ebe9c884032f1d8c
Yakir Oren <yakiroren@gmail.com>
2021-08-29 17:22:21 +0300

    change to back ticks

--- consts/consts.go
+++ consts/consts.go
@@ -18,7 +18,32 @@ const (
 
 	ParamPlaceholderPrefix = "Example: "
 
-	READMETemplate = "## blink-{{ .Describe.Name }}\n\n> {{ .Describe.Description }}\n\n{{range .GetActions}}\n## {{.Name }}\n* {{.Description }}\n<table>\n<caption>Action Parameters</caption>\n  <thead>\n    <tr>\n        <th>Param Name</th>\n        <th>Param Description</th>\n    </tr>\n  </thead>\n  <tbody>\n    <tr>{{ range $name, $param := .Parameters}}\n       <tr>\n           <td>{{ $name }}</td>\n           <td>{{ $param.Description }}</td>\n       </tr>{{ end}}\n    </tr>\n  </tbody>\n</table>\n{{ end}}"
+	READMETemplate = `
+## blink-{{ .Describe.Name }}
+
+> {{ .Describe.Description }}
+
+{{range .GetActions}}
+## {{.Name }}
+* {{.Description }}
+<table>
+<caption>Action Parameters</caption>
+  <thead>
+    <tr>
+        <th>Param Name</th>
+        <th>Param Description</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>{{ range $name, $param := .Parameters}}
+       <tr>
+           <td>{{ $name }}</td>
+           <td>{{ $param.Description }}</td>
+       </tr>{{ end}}
+    </tr>
+  </tbody>
+</table>
+{{ end}}`
 
 	README = "README.md"
 
