b04d7dbd5cbafe5cf897af07bf4d3fd8f25e7e74
Yakir Oren <39521848+YakirOren@users.noreply.github.com>
2021-10-03 16:22:01 +0300

    path params from connection

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -26,6 +26,7 @@ var (
 
 type HeaderValuePrefixes map[string]string
 type HeaderAlias map[string]string
+type PathParams []string
 
 type JSONMap interface{}
 
@@ -43,6 +44,7 @@ type openApiPlugin struct {
 	ValidateResponse    func(Result) (bool, []byte)
 	HeaderValuePrefixes HeaderValuePrefixes
 	HeaderAlias         HeaderAlias
+	PathParams          PathParams
 }
 
 type PluginMetadata struct {
@@ -53,6 +55,7 @@ type PluginMetadata struct {
 	Tags                []string
 	HeaderValuePrefixes HeaderValuePrefixes
 	HeaderAlias         HeaderAlias
+	PathParams          PathParams
 }
 
 type PluginChecks struct {
@@ -96,9 +99,7 @@ func (p *openApiPlugin) TestCredentials(conn map[string]connections.ConnectionIn
 }
 
 func (p *openApiPlugin) ActionExist(actionName string) bool {
-	log.Info(actionName)
 	for _, val := range p.actions {
-		log.Info(val.Name)
 		if val.Name == actionName {
 			return true
 		}
@@ -206,7 +207,23 @@ func (p *openApiPlugin) parseActionRequest(actionContext *plugin.ActionContext, 
 
 	// replace the raw parameters with their alias.
 	requestParameters := mask.ReplaceActionParametersAliases(actionName, rawParameters)
-	requestUrl = GetRequestUrl(actionContext, p.Describe().Provider)
+
+	provider := p.Describe().Provider
+
+	requestUrl = GetRequestUrl(actionContext, provider)
+
+	// add to request parameters
+
+	paramsFromConnection, err := GetPathParamsFromConnection(actionContext, provider, p.PathParams)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range paramsFromConnection {
+		requestParameters[k] = v
+	}
+
 	requestPath := parsePathParams(requestParameters, operation, operation.Path)
 	operationUrl, err := url.Parse(requestUrl + requestPath)
 
@@ -240,6 +257,37 @@ func (p *openApiPlugin) parseActionRequest(actionContext *plugin.ActionContext, 
 	parseQueryParams(requestParameters, operation, request)
 
 	return request, nil
+}
+
+func GetPathParamsFromConnection(actionContext *plugin.ActionContext, provider string, params PathParams) (map[string]string, error) {
+
+	connection, err := GetCredentials(actionContext, provider)
+	if err != nil {
+		return nil, err
+	}
+
+	paramsFromConnection := map[string]string{}
+
+	for header, headerValue := range connection {
+		if headerValueString, ok := headerValue.(string); ok {
+			if stringInSlice(header, params) {
+				paramsFromConnection[header] = headerValueString
+			}
+
+		}
+
+	}
+
+	return paramsFromConnection, nil
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if strings.EqualFold(b, a) {
+			return true
+		}
+	}
+	return false
 }
 
 func NewOpenApiPlugin(connectionTypes map[string]connections.Connection, meta PluginMetadata, checks PluginChecks) (*openApiPlugin, error) {
@@ -337,6 +385,7 @@ func NewOpenApiPlugin(connectionTypes map[string]connections.Connection, meta Pl
 		actions:             actions,
 		HeaderValuePrefixes: meta.HeaderValuePrefixes,
 		HeaderAlias:         meta.HeaderAlias,
+		PathParams:          meta.PathParams,
 		description: plugin.Description{
 			Name:        meta.Name,
 			Description: openApi.Info.Description,
